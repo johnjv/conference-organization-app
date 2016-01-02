d3ec4719c8c8dd9ac3bec30d8fc13ead8276cb6c
Justin Varghese John <justinjohn107@gmail.com>
2016-01-02 08:46:58 -0600

    Add web client id
    set up oath credentials in google developer console

--- settings.py
+++ settings.py
@@ -12,7 +12,7 @@ created/forked from conference.py by wesc on 2014 may 24
 
 # Replace the following lines with client IDs obtained from the APIs
 # Console or Cloud Console.
-WEB_CLIENT_ID = 'replace with Web client ID'
+WEB_CLIENT_ID = '941403918060-dirll99aap327iemqsepfh3eaccg3pad.apps.googleusercontent.com'
 ANDROID_CLIENT_ID = 'replace with Android client ID'
 IOS_CLIENT_ID = 'replace with iOS client ID'
 ANDROID_AUDIENCE = WEB_CLIENT_ID
